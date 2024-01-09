cc6f0a728dacb01cce403d8dc62755fb22146768
denis rodrigues dos santos <denisrodrigues.ita@gmail.com>
2024-01-09 19:04:57 -0300

    update files

--- dist/services/prisma.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.prisma = void 0;
-var _client = require("@prisma/client");
-var prisma = exports.prisma = new _client.PrismaClient();
